39fa502c15cdba6570641c3c7b821010dd035707
Ryan.cjw@gmail.com <ryan.cjw@gmail.com>
2018-12-06 15:54:18 -0800

    Merged battery scripts, added script to fix wifi

--- scripts/pb/PowerStats.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-val=$(cat /sys/class/power_supply/battery/current_now)
-printf "Battery Current = %suA\n" "$val"
-
-val=$(cat /sys/class/power_supply/battery/voltage_now)
-printf "Battery Voltage = %suV\n" "$val"
