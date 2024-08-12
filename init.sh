597873d95ddfeefbb0497db796c139dceab133e7
o7-machinehum <ryan.cjw@gmail.com>
2024-08-12 14:10:41 +0000

    Moved around some of the furniture.

--- init.sh
+++ init.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 repo_dir=$(pwd)
-ln -s $repo_dir/dotfiles/.vimrc ~/.vimrc
 ln -s $repo_dir/dotfiles/.Xmodmap ~/.Xmodmap
 ln -s $repo_dir/dotfiles/.xinitrc ~/.xinitrc
 ln -s $repo_dir/dotfiles/.bashrc ~/.bashrc
+ln -s $repo_dir/dotfiles/.Xresources ~/.Xresources
+
+mkdir -p ~/.config/vim
+mkdir -p ~/.config/i3
+mkdir -p ~/.config/alacritty
 
 ln -s $repo_dir/dotfiles/i3/config ~/.config/i3/config
+ln -s $repo_dir/dotfiles/alacritty.toml ~/.config/alacritty/alacritty.toml
+ln -s $repo_dir/dotfiles/vimrc ~/.config/vim/vimrc
